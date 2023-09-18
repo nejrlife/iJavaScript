1168b26b9fc92b172faee1eddba6d1be56c9fc03
Nixon Edora Jr <NixonJr.Edora@lumen.com>
2023-09-18 23:57:40 +0800

    Unit testing

---
+++ jest.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.jsx?$': 'babel-jest',
+  },
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '\\.(css|less)$': '<rootDir>/test/jest/styleMock.js',
+  }
+};
