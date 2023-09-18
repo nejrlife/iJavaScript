3cf162fb1d763a19933f4e66ca73d4d0f968b3a7
Nixon Edora Jr <NixonJr.Edora@lumen.com>
2023-09-18 15:25:40 +0800

    Progress push

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,37 @@
-import './Header.less';
+import "./Header.less";
+import { FiLogOut } from "react-icons/fi"
+import { connect } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { IS_AUTHENTICATED_CLEARDETAILS } from "../sagas/constants";
 
-const Header = () => {
+const Header = (props: any) => {
+  const {
+    dispatch  
+  } = props;
+  const navigate = useNavigate();
+
+  const handleLogout = (event: any) => {
+    event.preventDefault();
+    dispatch({
+      type: IS_AUTHENTICATED_CLEARDETAILS
+    });
+    sessionStorage.removeItem('userToken');
+    navigate('/login');
+  }
   return (
     <div className='headerFlex'>
       <p className='js_framework'>React.js</p>
       <p className='heading'>iJavaScript</p>
+      <div className='logoutFlex'>
+        {props.isUserAuthenticated && <FiLogOut
+          onClick={handleLogout}
+          color='#FFFFFF'
+        />}  
+      </div>  
     </div>
   )
 }
-export default Header;
+const mapStateToProps = (state:any) => ({
+  isUserAuthenticated: state.authenticateUser.isUserAuthenticated
+});
+export default connect(mapStateToProps, null)(Header);
--- src/components/SideNavBar.tsx
+++ src/components/SideNavBar.tsx
@@ -1,25 +1,7 @@
 import "./SideNavBar.less";
 import { NavLink } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
-import { IS_AUTHENTICATED_CLEARDETAILS } from "../sagas/constants";
-import { connect } from "react-redux";
 
-const SideNavBar = (props: any) => {
-
-  const {
-    dispatch  
-  } = props;
-
-  const navigate = useNavigate();
-
-  const handleLogout = (event: any) => {
-    event.preventDefault();
-    dispatch({
-      type: IS_AUTHENTICATED_CLEARDETAILS
-    });
-    sessionStorage.removeItem('userToken');
-    navigate('/login');
-  }
+const SideNavBar = () => {
   return (
     <>
       <div className='leftSider'>
@@ -40,14 +22,10 @@ const SideNavBar = (props: any) => {
             <li>
               <NavLink to="/home/team">TEAM</NavLink>
             </li>
-            {/* <hr />
-            <li>
-              <a href="#" onClick={handleLogout}>LOGOUT</a>
-            </li> */}
           </nav>
         </ul>
       </div>
     </>
   )
 }
-export default connect(null, null)(SideNavBar);
+export default SideNavBar;
